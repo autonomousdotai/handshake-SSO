eb691b44736a7d808e528de5127899fe675eda2e
hieuqautonomous <hieu.q@autonomous.nyc>
2018-05-28 13:07:18 +0700

    update cors

--- middlewares/cors.go
+++ middlewares/cors.go
@@ -7,9 +7,9 @@ import (
 
 func CORSMiddleware() gin.HandlerFunc {
     config := cors.DefaultConfig()
-    config.AllowOrigins = []string{"https://stag-handshake.autonomous.ai", "http://localhost:8080", "http://0.0.0.0:8080"}
     config.AllowHeaders = []string{"Content-Type", "Origin", "Payload", "*"}
     config.ExposeHeaders = []string{"Content-Length"}
     config.AllowCredentials = true
+    config.AllowAllOrigins = true
     return cors.New(config)
 }
