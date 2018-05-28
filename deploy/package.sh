1d5f753e483f983baeaeecd994a7dc51dc9e4d65
hieuqautonomous <hieu.q@autonomous.nyc>
2018-05-28 13:20:29 +0700

    add install package sh

---
+++ deploy/package.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+go get github.com/gin-gonic/gin
+go get github.com/jinzhu/gorm
+go get github.com/segmentio/ksuid
+go get github.com/spf13/viper
+go get github.com/go-sql-driver/mysql
+go get github.com/gin-contrib/cors
