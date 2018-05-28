275ebda41e3f16a8d7b995ce3dad23ba4c8bfa4d
hieuqautonomous <hieu.q@autonomous.nyc>
2018-05-28 13:21:31 +0700

    update cors

--- middlewares/cors.go
+++ middlewares/cors.go
@@ -1,16 +1,18 @@
 package middlewares
 
 import (
-    "github.com/gin-gonic/gin"
-    "github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/cors"
 )
 
 func CORSMiddleware() gin.HandlerFunc {
-    config := cors.DefaultConfig()
-    config.AllowOrigins = []string{"*"}
-    config.AllowHeaders = []string{"Content-Type", "Origin", "Payload", "*"}
-    config.ExposeHeaders = []string{"Content-Length"}
-    config.AllowCredentials = true
-    config.AllowAllOrigins = true
-    return cors.New(config)
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"*"}
+	config.AllowHeaders = []string{"Content-Type", "Origin", "Payload", "*"}
+	config.ExposeHeaders = []string{"Content-Length"}
+	config.AllowCredentials = true
+	config.AllowOriginFunc = func(origin string) bool {
+		return true
+	}
+	return cors.New(config)
 }
