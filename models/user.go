7b78ec787f4b960238e3faeb1947dd4d6f2ae6f7
0xgenoskawa <stone@Stones-MacBook-Pro.local>
2018-05-30 12:01:26 +0700

    fix user json reward_wallet_addresses

--- models/user.go
+++ models/user.go
@@ -13,7 +13,7 @@ type User struct {
     Status int `gorm:"column:status;default:1;" json:"status"`
     CardID string `gorm:"column:card_id;" json:"card_id"`
     CardVerified int `gorm:"column:card_verified;default:0" json:"card_verified"`
-    RewardWalletAddresses string `gorm:"column:reward_wallet_addresses;size:1000' json:"reward_wallet_addresses"`
+    RewardWalletAddresses string `gorm:"column:reward_wallet_addresses;size:1000" json:"reward_wallet_addresses"`
 }
 
 func (u User) TableName() string {
