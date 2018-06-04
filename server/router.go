8dca0e3bc6a1831f14df4421fe87d2f8a52a5d80
0xgenoskawa <stone@Stones-MacBook-Pro.local>
2018-06-04 18:50:53 +0700

    add Fcm-Token to header fwd

--- server/router.go
+++ server/router.go
@@ -110,7 +110,8 @@ func Forwarding(c *gin.Context, endpoint *interface{}, path string) {
             v := c.GetHeader(k)
             req.Header.Set(k, v)
         }
-        req.Header.Set("uid", strconv.FormatUint(uint64((user.(models.User)).ID), 10))
+        req.Header.Set("Uid", strconv.FormatUint(uint64((user.(models.User)).ID), 10))
+        req.Header.Set("Fcm-Token", (user.(model.User)).FCMToken)
     }
     proxy := &httputil.ReverseProxy{Director: director} 
     proxy.Transport = &ForwardingTransport{}
