7a6de74147585b86cbe5864ab343e0a2e29cd232
0xngoubou <phuong@autonomous.nyc>
2018-10-16 10:36:28 +0700

    update filed store

--- controllers/store.go
+++ controllers/store.go
@@ -82,7 +82,7 @@ func (u StoreController) Create(c *gin.Context) {
 				Status: status,
 				StoreName: store_name,
 				StoreID: store_id,
-				WalletReceive: wallets_receive,
+				WalletsReceive: wallets_receive,
 				ConfirmURL: confirm_url,
 				UserID: userModel.ID,
 				ReceiveEmail: receive_email,
@@ -179,7 +179,7 @@ func (i StoreController) UpdateStore(c *gin.Context) {
 		store.Status = status
 	}
 	if wallets_receive != "_" {
-		store.WalletReceive = wallets_receive
+		store.WalletsReceive = wallets_receive
 	}
 	if confirm_url != "_" {
 		store.ConfirmURL = confirm_url
--- models/store.go
+++ models/store.go
@@ -9,10 +9,11 @@ type Store struct {
 	UserID       	uint   `gorm:"column:user_id" json:"user_id"`
 	StoreID      	string `gorm:"column:store_id;unique;default:NULL" json:"store_id"`
 	StoreName  	string `gorm:"column:store_name" json:"store_name"`
+	WalletsReceive 	string `gorm:"column:wallets_receive;size:1000" json:"wallets_receive"`
 	ConfirmURL	string `gorm:"column:confirm_url" json:"confirm_url"`
 	Status      	int    `gorm:"column:status;default:0" json:"status"`
 	ReceiveEmail    string `gorm:"column:receive_email" json:"receive_email"`
-	WalletReceive   string `gorm:"column:wallet_receive;size:1000" json:"wallet_receive"`
+
 }
 
 func (u Store) TableName() string {
